63dfa1d444421078c3311d3ba2602b9daa249a3b
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-10-30 18:44:07 -0400

    added folder and files for dragon game

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,6 +1,10 @@
 var content = document.getElementById('content');
 
-var ticTacToe = "Replace this with your own abstraction of Tic Tac Toe"
+var ticTacToe = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+];
 
 content.innerHTML = renderGame(ticTacToe);
 
@@ -9,7 +13,6 @@ content.innerHTML = renderGame(ticTacToe);
 
 function renderGame(game) {
     // Change this render function to use the "game" parameter
-
     return `
         <div class="container d-flex flex-column justify-content-start align-items-center">
             <h4>It's player O's turn!</h4>
